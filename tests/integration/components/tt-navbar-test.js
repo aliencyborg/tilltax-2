6d1571b3689a15f6a57876fe406badc01747cbcc
Benjamin Rosas <ben@aliencyb.org>
2019-02-11 01:55:07 +0000

    lint

--- tests/integration/components/tt-navbar-test.js
+++ tests/integration/components/tt-navbar-test.js
@@ -1,26 +1,26 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit'
+import { setupRenderingTest } from 'ember-qunit'
+import { render } from '@ember/test-helpers'
+import hbs from 'htmlbars-inline-precompile'
 
 module('Integration | Component | tt-navbar', function(hooks) {
-  setupRenderingTest(hooks);
+  setupRenderingTest(hooks)
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{tt-navbar}}`);
+    await render(hbs`{{tt-navbar}}`)
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(this.element.textContent.trim(), '')
 
     // Template block usage:
     await render(hbs`
       {{#tt-navbar}}
         template block text
       {{/tt-navbar}}
-    `);
+    `)
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
-});
+    assert.equal(this.element.textContent.trim(), 'template block text')
+  })
+})
