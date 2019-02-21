04e98f51dfde5d5317717386e44b92aeb01d84c6
Benjamin Rosas <ben@aliencyb.org>
2019-02-21 00:37:10 +0000

    hide the navbar and footer on splash

--- app/components/tt-frame.js
+++ app/components/tt-frame.js
@@ -4,16 +4,15 @@ import { computed } from '@ember/object'
 
 export default Component.extend({
   router: service(),
-  classNameBindings: ['isHidden:hidden'],
   classNames: ['flex', 'flex-1', 'flex-col'],
   elementId: 'tt-frame',
 
-  isHidden: computed('router.currentRoute', function() {
+  showElements: computed('router.currentRoute', function() {
     const router = this.get('router')
     const {
       currentRoute: { name }
     } = router
 
-    return name === 'index'
+    return name !== 'index'
   })
 })
