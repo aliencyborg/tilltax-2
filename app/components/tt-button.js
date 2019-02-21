4efb9d085f8b914050cf8e707023dfa07c429698
Benjamin Rosas <ben@aliencyb.org>
2019-02-21 05:12:53 +0000

    contact-us button; some index tweaks

---
+++ app/components/tt-button.js
@@ -0,0 +1,12 @@
+import Component from '@ember/component'
+
+export default Component.extend({
+  classNames: [
+    'bg-blue',
+    'font-bold',
+    'hover:bg-blue-dark',
+    'px-4',
+    'py-2',
+    'text-white'
+  ]
+})
