d5665cb6b0b47699751df05996e9ea96089a6f56
Benjamin Rosas <ben@aliencyb.org>
2019-04-08 10:10:36 -0700

    point to regular production api

--- config/environment.js
+++ config/environment.js
@@ -49,7 +49,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.DS.host = 'https://test-api.tilltax.com'
+    ENV.DS.host = 'https://api.tilltax.com'
   }
 
   return ENV
