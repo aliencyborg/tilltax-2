d57b6a3c41cb2e6e423a00373d55b1c67b865175
Benjamin Rosas <ben@aliencyb.org>
2019-05-02 01:18:36 -0500

    syntax error

--- app/services/beta-site.js
+++ app/services/beta-site.js
@@ -1,7 +1,7 @@
 import Service from '@ember/service'
-import { APP } from 'tilltax/config/environment'
+import ENV from 'tilltax/config/environment'
 
 export default class BetaSiteService extends Service {
   // TODO: read a ?beta query param as well
-  isBeta: APP.betaSite
+  isBeta = ENV.APP.betaSite
 }
