bb9f5d8d4128629c8674f0230de8e1ef36611836
Benjamin Rosas <ben@aliencyb.org>
2019-05-07 20:49:14 -0500

    add gtm to index.html

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,18 +1,18 @@
-import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route'
+import { inject as service } from '@ember/service'
 
 export default Route.extend({
   metrics: service(),
   router: service(),
 
   init() {
-    this._super(...arguments);
+    this._super(...arguments)
 
     this.on('routeDidChange', () => {
-      const page = this.router.currentURL;
-      const title = this.router.currentRouteName || 'unknown';
+      const page = this.router.currentURL
+      const title = this.router.currentRouteName || 'unknown'
 
-      this.metrics.trackPage({ page, title });
+      this.metrics.trackPage({ page, title })
     })
   }
-});
+})
